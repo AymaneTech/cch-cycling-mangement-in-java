92f698bc6fdff81b168a85531e4d6606530f5714
geek <elmainiaymane03@gmail.com>
2024-10-17 19:27:16 +0100

    create new dto response for the passed stage result

--- src/main/java/com/wora/comptetition/application/dto/response/PassedStageResponseDto.java
+++ src/main/java/com/wora/comptetition/application/dto/response/PassedStageResponseDto.java
@@ -1,14 +1,16 @@
 package com.wora.comptetition.application.dto.response;
 
 import com.wora.comptetition.domain.valueObject.StageId;
+import com.wora.rider.application.dto.response.RiderResponseDto;
 import com.wora.rider.domain.valueObject.RiderId;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.time.Duration;
 
-public record PassedStageResponseDto(@NotNull StageId stageId,
-                                     @NotNull RiderId riderId,
-                                     @NotNull @Positive Duration duration
+public record PassedStageResponseDto(@NotNull StageResponseDto stage,
+                                     @NotNull RiderResponseDto rider,
+                                     @NotNull @Positive Duration duration,
+                                     @NotNull @Positive Integer position
 ) {
 }
