ea168f4176b406f5e791d02e7782c2c1e30a6754
geek <elmainiaymane03@gmail.com>
2024-10-19 21:28:40 +0100

    remove unused env file reader and also the .env.example

--- src/main/java/com/wora/common/application/service/CrudService.java
+++ src/main/java/com/wora/common/application/service/CrudService.java
@@ -1,9 +1,5 @@
 package com.wora.common.application.service;
 
-import com.wora.rider.application.dto.request.RiderRequestDto;
-import com.wora.rider.application.dto.response.RiderResponseDto;
-import com.wora.rider.domain.valueObject.RiderId;
-
 import java.util.List;
 
 public interface CrudService<Entity, Id, RequestDto, ResponseDto> {
