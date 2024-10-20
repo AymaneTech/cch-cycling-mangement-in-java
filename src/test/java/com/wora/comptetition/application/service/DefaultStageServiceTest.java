46c17e7880695e2bee345f942ec9b23e9e772335
geek <elmainiaymane03@gmail.com>
2024-10-20 13:15:00 +0100

    write unit testing for create, update, delete methods of stage service

--- src/test/java/com/wora/comptetition/application/service/DefaultStageServiceTest.java
+++ src/test/java/com/wora/comptetition/application/service/DefaultStageServiceTest.java
@@ -1,6 +1,7 @@
 package com.wora.comptetition.application.service;
 
 import com.wora.common.domain.exception.EntityNotFoundException;
+import com.wora.comptetition.application.dto.request.StageRequestDto;
 import com.wora.comptetition.application.dto.response.CompetitionResponseDto;
 import com.wora.comptetition.application.dto.response.StageResponseDto;
 import com.wora.comptetition.application.service.impl.DefaultStageService;
@@ -27,8 +28,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Default stage service")
@@ -49,7 +49,7 @@ class DefaultStageServiceTest {
     @BeforeEach
     void setup() {
         this.sut = new DefaultStageService(repository, competitionRepository, mapper);
-        stage = new Stage(1, 30.3, "marrakech", "casablanca", LocalDate.now(), competition);
+        stage = new Stage(1, 30.3, "marrakech", "casablanca", LocalDate.now(), competition).setId(new StageId());
         competition = new Competition(new CompetitionId(), "maroc", LocalDate.now(), LocalDate.now().plusMonths(1));
     }
 
@@ -143,6 +143,129 @@ class DefaultStageServiceTest {
 
             assertEquals(stage.getId(), actual.id());
             assertNotNull(actual);
+        }
+    }
+
+    @DisplayName("create() method tests")
+    @Nested
+    class CreateTests {
+        private StageRequestDto dto;
+
+        @BeforeEach
+        void setup() {
+            dto = new StageRequestDto(23, 202.2, "marrakech", "safi", LocalDate.now(), competition.getId());
+        }
+
+        @Test
+        void create_ShouldThrowEntityNotFoundException_WhenGivenNotExistingCompetitionId() {
+
+            when(competitionRepository.findById(any(CompetitionId.class))).thenReturn(Optional.empty());
+
+            assertThrows(EntityNotFoundException.class, () -> sut.create(dto));
+        }
+
+        @Test
+        void create_ShouldReturnCreatedStage_WhenGivenValidRequest() {
+            when(competitionRepository.findById(any(CompetitionId.class))).thenReturn(Optional.of(competition));
+            when(mapper.map(any(StageRequestDto.class), eq(Stage.class))).thenReturn(stage);
+            when(repository.save(any(Stage.class))).thenReturn(stage);
+            when(mapper.map(any(Stage.class), eq(StageResponseDto.class)))
+                    .thenAnswer(invocation -> {
+                        Stage s = invocation.getArgument(0);
+                        return new StageResponseDto(s.getId(), s.getStageNumber(), s.getDistance(), s.getStartLocation(), s.getEndLocation(),
+                                s.getDate(), new CompetitionResponseDto(competition.getId(), competition.getName(),
+                                competition.getStartDate(), competition.getEndDate()));
+                    });
+
+            StageResponseDto actual = sut.create(dto);
+
+            assertNotNull(stage);
+            assertEquals(stage.getStageNumber(), actual.stageNumber());
+        }
+    }
+
+    @DisplayName("update() methods tests")
+    @Nested
+    class UpdateTests {
+        private StageRequestDto dto;
+
+        @BeforeEach
+        void setup() {
+            dto = new StageRequestDto(1, 210.5, "Paris", "Marseille", LocalDate.now().plusDays(1), competition.getId());
+        }
+
+        @DisplayName("Should return updated stage when given existing id")
+        @Test
+        void update_ShouldReturnUpdatedStageWhenGivenExistingId() {
+            Stage updatedStage = new Stage(dto.stageNumber(), dto.distance(), dto.startLocation(), dto.endLocation(), dto.date(), competition)
+                    .setId(stage.getId());
+
+            when(repository.findById(stage.getId())).thenReturn(Optional.of(stage));
+            when(competitionRepository.findById(any(CompetitionId.class))).thenReturn(Optional.of(competition));
+            doAnswer(invocation -> {
+                Stage s = invocation.getArgument(1);
+                s.setStageNumber(dto.stageNumber())
+                        .setDistance(dto.distance())
+                        .setStartLocation(dto.startLocation())
+                        .setEndLocation(dto.endLocation())
+                        .setDate(dto.date());
+                return null;
+            }).when(mapper).map(eq(dto), any(Stage.class));
+            when(mapper.map(any(Stage.class), eq(StageResponseDto.class)))
+                    .thenReturn(new StageResponseDto(updatedStage.getId(), updatedStage.getStageNumber(), updatedStage.getDistance(),
+                            updatedStage.getStartLocation(), updatedStage.getEndLocation(), updatedStage.getDate(),
+                            new CompetitionResponseDto(competition.getId(), competition.getName(), competition.getStartDate(), competition.getEndDate())));
+
+            StageResponseDto actual = sut.update(stage.getId(), dto);
+
+            assertEquals(stage.getId(), actual.id());
+            assertEquals(dto.stageNumber(), actual.stageNumber());
+            assertEquals(dto.distance(), actual.distance());
+            assertEquals(dto.startLocation(), actual.startLocation());
+            assertEquals(dto.endLocation(), actual.endLocation());
+            assertEquals(dto.date(), actual.date());
+            verify(repository).findById(any(StageId.class));
+        }
+
+        @DisplayName("Should throw entity not found exception when given not existing id")
+        @Test
+        void update_ShouldThrowEntityNotFoundException_WhenGivenNotExistingId() {
+            when(repository.findById(any(StageId.class))).thenReturn(Optional.empty());
+            assertThrows(EntityNotFoundException.class, () -> sut.update(new StageId(), dto));
+        }
+
+        @DisplayName("Should throw entity not found exception when given not existing competition id")
+        @Test
+        void update_ShouldThrowEntityNotFoundException_WhenGivenNotExistingCompetitionId() {
+            when(repository.findById(any(StageId.class))).thenReturn(Optional.of(stage));
+            when(competitionRepository.findById(any(CompetitionId.class))).thenReturn(Optional.empty());
+            assertThrows(EntityNotFoundException.class, () -> sut.update(new StageId(), dto));
+        }
+    }
+
+    @DisplayName("delete method tests")
+    @Nested
+    class DeleteTests {
+        @Test
+        void delete_ShouldThrowEntityNotFoundException_WhenGivenNotExistingId() {
+            StageId stageId = new StageId();
+
+            when(repository.existsById(any(StageId.class))).thenReturn(false);
+
+            assertThrows(EntityNotFoundException.class, () -> sut.delete(stageId));
+        }
+
+        @Test
+        void delete_ShouldDeleteStage_WhenGivenExistingId() {
+            StageId stageId = new StageId();
+
+            when(repository.existsById(any(StageId.class))).thenReturn(true);
+
+            sut.delete(stageId);
+
+            verify(repository).existsById(any(StageId.class));
+            verify(repository).softDeleteById(any(StageId.class));
+
         }
     }
 }
