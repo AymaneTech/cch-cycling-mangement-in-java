9af0db11e1d211ce0857a9762691a70db3e8142f
geek <elmainiaymane03@gmail.com>
2024-10-20 17:43:45 +0100

    finish crud operations of team controller

--- src/main/java/com/wora/rider/infrastructure/web/TeamController.java
+++ src/main/java/com/wora/rider/infrastructure/web/TeamController.java
@@ -1,15 +1,17 @@
 package com.wora.rider.infrastructure.web;
 
+import com.wora.rider.application.dto.request.TeamRequestDto;
 import com.wora.rider.application.dto.response.TeamResponseDto;
 import com.wora.rider.application.service.TeamService;
+import com.wora.rider.domain.valueObject.TeamId;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/teams")
@@ -18,11 +20,34 @@ public class TeamController {
     private final TeamService service;
 
     @GetMapping
-    @ResponseBody
     public ResponseEntity<List<TeamResponseDto>> findAll() {
         List<TeamResponseDto> teams = service.findAll();
-        System.out.println(teams);
         return ResponseEntity.ok(teams);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TeamResponseDto> findById(@PathVariable("id") UUID id) {
+        TeamResponseDto team = service.findById(new TeamId(id));
+        return ResponseEntity.ok(team);
+    }
+
+    @PostMapping
+    public ResponseEntity<TeamResponseDto> create(@RequestBody @Valid TeamRequestDto dto) {
+        TeamResponseDto team = service.create(dto);
+        return new ResponseEntity<>(team, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TeamResponseDto> update(@PathVariable("id") UUID id, @RequestBody @Valid TeamRequestDto dto) {
+        TeamResponseDto team = service.update(new TeamId(id), dto);
+        return ResponseEntity.ok(team);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") UUID id) {
+        service.delete(new TeamId(id));
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
