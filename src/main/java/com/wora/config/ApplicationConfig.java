6045139d6ccc3c3aed89a8beddd598da93ffc98c
geek <elmainiaymane03@gmail.com>
2024-10-24 20:06:26 +0100

    add schedule configuration

--- src/main/java/com/wora/config/ApplicationConfig.java
+++ src/main/java/com/wora/config/ApplicationConfig.java
@@ -1,11 +1,17 @@
 package com.wora.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
 
 @Configuration
-@ComponentScan("com.wora")
+@ComponentScan(basePackages = "com.wora")
 @Import({PersistenceConfig.class, WebMvcConfig.class})
 public class ApplicationConfig {
+    @Bean
+    public ScheduledAnnotationBeanPostProcessor scheduledAnnotationBeanPostProcessor() {
+        return new ScheduledAnnotationBeanPostProcessor();
+    }
 }
